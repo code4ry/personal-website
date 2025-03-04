5fc2ad9ab3fcdfd599865bdf84f38df6cf4951f3
code4ry <140927679+code4ry@users.noreply.github.com>
2025-03-04 11:19:29 -0500

    Update db.js
    Forgot to hash passcode

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ let dbConnection;
 
 module.exports = {
     connectToDb: (cb) => {
-        MongoClient.connect('mongodb+srv://rylee1689:hWKjCxsJIPREjeHv@clusterryan.friyn.mongodb.net/')
+        MongoClient.connect('mongodb+srv://rylee1689:@clusterryan.friyn.mongodb.net/')
             .then((client) => {
                 dbConnection = client.db('db');
                 return cb();
